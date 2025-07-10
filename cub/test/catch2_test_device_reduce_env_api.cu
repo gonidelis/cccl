e127acc16a2a7d5538b6fba7a3d75be7cb41f95c
gonidelis <ggonidelis@nvidia.com>
2025-07-10 07:46:16 -0700

    env based TransformReduce gpu_to_gpu

--- cub/test/catch2_test_device_reduce_env_api.cu
+++ cub/test/catch2_test_device_reduce_env_api.cu
@@ -15,7 +15,7 @@
 
 C2H_TEST("cub::DeviceReduce::Reduce accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin reduce-env-determinism
   auto op     = cuda::std::plus{};
@@ -87,7 +87,7 @@ C2H_TEST("cub::DeviceReduce::Sum accepts stream", "[reduce][env]")
 
 C2H_TEST("cub::DeviceReduce::Min accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin min-env-determinism
   auto input  = c2h::device_vector<float>{0.0f, 1.0f, 2.0f, 3.0f};
@@ -136,7 +136,7 @@ C2H_TEST("cub::DeviceReduce::Min accepts stream", "[reduce][env]")
 
 C2H_TEST("cub::DeviceReduce::Max accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin max-env-determinism
   auto input  = c2h::device_vector<float>{0.0f, 1.0f, 2.0f, 3.0f};
@@ -171,7 +171,7 @@ C2H_TEST("cub::DeviceReduce::Max accepts stream", "[reduce][env]")
 
 C2H_TEST("cub::DeviceReduce::ArgMin accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin argmin-env-determinism
   auto input        = c2h::device_vector<float>{3.0f, 1.0f, 4.0f, 0.0f, 2.0f};
@@ -212,7 +212,7 @@ C2H_TEST("cub::DeviceReduce::ArgMin accepts stream", "[reduce][env]")
 
 C2H_TEST("cub::DeviceReduce::ArgMax accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin argmax-env-determinism
   auto input        = c2h::device_vector<float>{3.0f, 1.0f, 4.0f, 0.0f, 2.0f};
@@ -262,7 +262,7 @@ struct square_t
 
 C2H_TEST("cub::DeviceReduce::TransformReduce accepts determinism requirements", "[reduce][env]")
 {
-  // TODO(gevtushenko): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
+  // TODO(gonidelis): replace `run_to_run` with `gpu_to_gpu` once RFA unwraps contiguous iterators
 
   // example-begin transform-reduce-env-determinism
   auto input  = c2h::device_vector<float>{1.0f, 2.0f, 3.0f, 4.0f};
